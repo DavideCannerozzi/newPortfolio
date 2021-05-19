7267cab2af758d7bc4c2750984ecf1a89f735a4a
davide cannerozzi <davide851_@hotmail.com>
2021-05-19 12:18:18 +0200

    working progress on project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import MachwerkImg from '../images/machwerk-berlin.jpg'
 import SeteShopImg from '../images/sete-shop.jpg'
 import VueTemplateImg from '../images/vue-template.jpg'
 import WeatherAppImg from '../images/weather-app.jpg'
+import JewelryTemplateImg from '../images/jewelry-template.jpg'
 import PongImg from '../images/pong-img.jpg'
 import ContactFormImg from '../images/contact-form.jpg'
 
@@ -13,12 +14,12 @@ export default function Projects(props) {
 
    const Projects = [
       { image:SeteShopImg, title:'SeteShop', content:'SeteShop is an e-commerce website built for one of my client.', alt:"seteShop", skills:'Wordpress, Javascript, CSS3', website:"https://www.seteshop.it/"},
-      { image:MachwerkImg, title:'ToDoList App', content:'Machwerk Is A Website', skills:'React.js GraphQL TailwindCss ContentfulCMS'},
-      { image:VueTemplateImg, title:'A Template For Photographers', content:'A Simple And Clean Template For Photographers', skills:'Vue.js Vuetify', website:"https://photo-portfolio-vue.netlify.app/"},
-      { image:PongImg, title:'Pong Game Python', content:'Game', skills:'Python'},
-      { image:VueTemplateImg, title:'jewelry Template', content:'A Clean Template for your jewelry shop', skills:'Bootstrap 4'},
-      { image:ContactFormImg, title: 'Contact Form', content:'Contact form', skills:'Html Css Javascript', website:"https://contact-form-js-validation.netlify.app/"},
-      { image:WeatherAppImg, title:'Weather App', website:"https://weather-js-project.netlify.app/"}
+      { image:MachwerkImg, title:'Machwerk Website Berlin,Germany', content:'Machwerk Is A Website', skills:'React.js GraphQL TailwindCss ContentfulCMS'},
+      { image:VueTemplateImg, title:'A Template For Photographers', content:'A Simple And Clean Template For Photographers', skills:'Vue.js Vuetify', website:"https://photo-portfolio-vue.netlify.app/", code:"https://github.com/DavideCannerozzi/PhotoPortfolio"},
+      { image:PongImg, title:'Pong Game Python', content:'The famous Pong game made with Python and Turtle.py ', skills:'Python', code:"https://github.com/DavideCannerozzi/PongGame"},
+      { image:JewelryTemplateImg, title:'jewelry Template', content:'A Clean Template For Your Jewelry Shop', skills:'Bootstrap 4 SASS', code:'https://github.com/DavideCannerozzi/Jewelry-Template'},
+      { image:ContactFormImg, title: 'Contact Form', content:'A Contact Form Validation Using Javascript', skills:'Html Css Javascript', website:"https://contact-form-js-validation.netlify.app/", code:"https://github.com/DavideCannerozzi/Contact-Form"},
+      { image:WeatherAppImg, title:'Weather App', content:"A Weather App showing the weather based on your location", skills:"Javascript Geolocation API" ,website:"https://weather-js-project.netlify.app/",code:"https://github.com/DavideCannerozzi/Weather-App"}
    ]
    
    return (
@@ -30,7 +31,7 @@ export default function Projects(props) {
          </div>
          <Bar />
          <div className="project-container p-8">
-           { Projects.map( project =>(<SingleProject image={project.image} title={project.title} content={project.content} skill={project.skills} alt={props.alt} linkWeb={project.website}/> )  )}
+           { Projects.map( project =>(<SingleProject image={project.image} title={project.title} content={project.content} skill={project.skills} alt={props.alt} linkWeb={project.website} code={project.code}/> )  )}
          </div>
       </div>
    )
--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -8,15 +8,14 @@ export default function SingleProject(props){
             <div className="project-img w-full">
                   <img src={props.image} alt={props.alt}/>
             </div>
-            <div className="project-description_p mt-14 w-full shadow-xl p-4">
+            <div className="project-description_p mt-14 w-full shadow-xl p-8">
                   <h3 className="underline text-lg">{props.title}</h3>
                   <p className="mt-6">{props.content}</p>
                   <span className="inline-block mt-8 mb-10">{props.skill}</span>
                   <div className="btn-projects">
-                        <button className="p-4 mr-12"><a href={props.linkWeb} target="_blank" rel="noreferrer">Live Project</a></button>
-                        <button className="p-4"><a href={props.linkWeb} target="_blank" rel="noreferrer">View Code</a></button>
+                        <button className="p-4 md:mr-12"><a href={props.linkWeb} target="_blank" rel="noreferrer">Live Project</a></button>
+                        <button className="p-4"><a href={props.code} target="_blank" rel="noreferrer">View Code</a></button>
                   </div>
-                 
             </div>
       </div>
       )
