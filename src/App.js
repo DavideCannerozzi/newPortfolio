7dfd22af43894e9ea3d41c82df16a1afc6f8cdc1
davide cannerozzi <davide851_@hotmail.com>
2021-06-15 15:56:36 +0200

    about page description

--- src/App.js
+++ src/App.js
@@ -44,9 +44,8 @@ export default function App() {
             />
             <Route path='/About' render={ () => <About 
               header="About Davide" 
-              aboutMeFirst = "Ciao, I am Davide Cannerozzi, a front-end developer from Italy. My passion for web development started back in 2018. After starting with Wordpress I started studying Html Css Javascript. More than one year ago i started with React.js and Vue.js"
-              aboutMeSecond = "In March 2019 ..... , mainly React.js, while i was working for a company in Berlin,Germany. If you want to know more about it check out my Project page.I also write about programming on my blog"
-              aboutMeThird = "I am currently working as a freelance, but I am always open to new opportunities, so don't hesitate to contact me.I am open to relocate."
+              aboutMeFirst = "Ciao, I am Davide Cannerozzi, a front-end developer from Italy. I am passionate about everything related to the web and programming."
+              aboutMeSecond = "I am currently working as a freelance, but I am always open to new opportunities, so don't hesitate to contact me.I am available to relocate."
               subHeader="looking for a developer?"/>
             }
             />
--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -9,7 +9,7 @@ export default function ContactMe(){
       { id:3, href:"https://github.com/DavideCannerozzi", class:'fab fa-2x fa-linkedin linkedin ml-8' }
    ]
       return(
-      <div className="md:text-left text-center md:flex">
+      <div className="md:text-left text-center md:flex mt-8">
          <a href="mailto:d.cannerozzi@gmail.com"><button className="rounded-md transition duration-500     contact-btn p-4 mt-6">Contact Me</button></a>
             <ul className="mt-10 ml-12 flex md:justify-start">
                { socialIcons.map( SocialIcon =>(<li className={SocialIcon.id}><a href={SocialIcon.href} target="_blank" rel="noreferrer"><i className={SocialIcon.class}></i></a></li>)) }
